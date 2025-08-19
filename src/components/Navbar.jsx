2f8bfe3a6e00e81cdba6461b2f7e16041f5c4c06
pereruannabaala <pereruannabaala@gmail.com>
2025-08-19 15:19:14 +0300

    push code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
+import logo from '../assets/react-logo.png'
 export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src="src/assets/react-logo.png" alt="React logo" />
+                <img src={logo} alt="React logo" />
                 <span>ReactFacts</span>
             </nav>
         </header>
